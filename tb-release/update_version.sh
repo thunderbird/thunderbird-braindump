eb44473bba1ccd9533ef5ca465f4cdb7819bfe5c
Rob Lemley <rob@thunderbird.net>
2024-10-15 11:44:04 -0400

    Add support to handle esr versions correctly.

--- tb-release/update_version.sh
+++ tb-release/update_version.sh
@@ -5,11 +5,14 @@ if [[ ! -f mail/config/version.txt ]]; then
   exit 1
 fi
 
-if [[ -n "$1" ]]; then
-  echo "$1" > mail/config/version.txt
-  echo "$1" > mail/config/version_display.txt
+VD="$1"
+V="${1%%esr}"
 
-  hg commit mail/config/version.txt mail/config/version_display.txt -m "No bug - Set version ${1} for release. r+a=release"
+if [[ -n "$1" ]]; then
+  echo "$V" > mail/config/version.txt
+  echo "$VD" > mail/config/version_display.txt
+
+  hg commit mail/config/version.txt mail/config/version_display.txt -m "No bug - Set version ${VD} for release. r+a=release"
 else
   echo "No version given."
   exit 2
